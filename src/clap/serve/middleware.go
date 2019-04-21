5ae06bcef39ec8e663cbca5d6cb017c88c3e830a
for2B <798362701@qq.com>
2019-04-21 10:20:20 +0800

    fix-changeFile

--- src/clap/serve/middleware.go
+++ src/clap/serve/middleware.go
@@ -3,17 +3,16 @@ package serve
 import (
 	"clap/staging/TBLogger"
 	"net/http"
-	"time"
 )
 
-func LogApiAccess(httpHandler http.HandlerFunc)http.HandlerFunc{
-	return func(w http.ResponseWriter, r *http.Request){
+func LogApiAccess(httpHandler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		TBLogger.TbLogger.Info("ApiAccess:",
-			"host:",r.Host,
-			"AccessApi:",r.URL.Path,
-			"Time:",time.Now().Format("2006-01-02 15:04:05"),
+			"host:", r.Host,
+			"AccessApi:", r.URL.Path,
+			"RemoteAddr", r.RemoteAddr,
 		)
-		httpHandler(w,r)
+		httpHandler(w, r)
 	}
 }
 
@@ -21,16 +20,16 @@ type DownFileAccessMid struct {
 	handlerFunc http.Handler
 }
 
-func (dfm DownFileAccessMid)ServeHTTP(w http.ResponseWriter, r *http.Request){
+func (dfm DownFileAccessMid) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	TBLogger.TbLogger.Info("ApiAccess",
-		"host:",r.Host,
-		"AccessApi:",r.URL.Path,
-		"Method",r.Method,
+		"host:", r.Host,
+		"AccessApi:", r.URL.Path,
+		"Method", r.Method,
 	)
-	dfm.handlerFunc.ServeHTTP(w,r)
+	dfm.handlerFunc.ServeHTTP(w, r)
 }
 
-func LogDownFileAccess(handlerFunc http.Handler) http.Handler{
+func LogDownFileAccess(handlerFunc http.Handler) http.Handler {
 	var dfm DownFileAccessMid
 	dfm.handlerFunc = handlerFunc
 	return dfm
